67aade02fb17cd60408162ac25bc17012c798898
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2015-01-13 23:38:01 +0100

    Added main_matmul.f90

---
+++ main_matmul.f90
@@ -0,0 +1,43 @@
+program test
+  implicit none
+  double precision, allocatable  :: A(:,:,:)
+  double precision, allocatable  :: B(:,:,:)
+  double precision, allocatable  :: C(:,:,:)
+  
+  integer                        :: n1,n2,n3
+  integer                        :: i,j,k
+  integer*8                      :: t0,t1, count_rate, count_max
+
+
+  print *,  'Enter n1, n2, n3'
+  read(*,*) n1,n2,n3
+
+  print *,  'n1=', n1
+  print *,  'n2=', n2
+  print *,  'n3=', n3
+
+  allocate (A(n1,n2,n3))
+  allocate (B(n1,n2,n3))
+  allocate (C(n1,n2,n3))
+
+  do j=1,n2
+    do i=1,n1
+      call random_number(A(i,j,1))
+    enddo
+  enddo
+
+  do k=2,n3
+    A(:,:,k) = A(:,:,1)
+  enddo
+  B = A
+
+  call system_clock(t0, count_rate, count_max)
+  do k=1,n3
+    C(:,:,k) = matmul(A(:,:,k),B(:,:,k))
+  enddo
+  call system_clock(t1, count_rate, count_max)
+  print *,  C(:,:,n3)
+  print *,  dble(t1-t0)/dble(count_rate), 'seconds'
+  deallocate(A,B,C)
+end
+
